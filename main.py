0c45578a6e1fc45c8f12db5e5c8d4e06e93d3611
Asad Mehmood <bcpe223053@cust.pk>
2024-09-08 21:38:48 +0500

    Minor Improvement

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import pandas
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_alphabet = {row.letter:row.code for (index, row) in df.iterrows()}
-word = input("Enter a word: ").upper()
-letter = word.split()
-list = [phonetic_alphabet[letter] for letter in word ]
-print(list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        list = [phonetic_alphabet[letter] for letter in word ]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(list)
+generate_phonetic()
